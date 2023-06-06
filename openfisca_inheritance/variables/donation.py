4920f01b3000efd52a3b92b00debca3a9a13b9e2
paulinebvt <pauline.bouvet@ipp.eu>
2023-06-06 17:17:20 +0200

    maj ajout _don

--- openfisca_inheritance/variables/donation.py
+++ openfisca_inheritance/variables/donation.py
@@ -1,6 +1,6 @@
-from openfisca_core.model_api import *
+import itertools
 
-from openfisca_inheritance.entities import Individu, donation  # Donations
+from openfisca_core.model_api import *
 
 from openfisca_inheritance.entities import Donation
 
@@ -38,17 +38,57 @@ from openfisca_inheritance.entities import Donation
 #
 #         is_enfant_donataire_holder = donation('is_enfant_donataire', period)
 #         return self.sum_by_entity(is_enfant_donataire_holder)
+class actif_de_communaute_don(Variable):
+    value_type = float
+    entity = Donation
+    label = "Actif de Communauté"
+    definition_period = ETERNITY
 
-class epoux_survivant(Variable):
+class actif_imposable_don(Variable):
+    value_type = float
+    entity = Donation
+    label = "Actif imposable"
+    definition_period = ETERNITY
+
+    def formula(donation, period, parameters):
+        part_epoux_don = donation('part_epoux_don', period)
+        actif_de_communaute_don = donation('actif_de_communaute_don', period)
+        passif_de_communaute_don = donation('passif_de_communaute_don', period)
+        actif_propre_don = donation('actif_propre_don', period)
+        passif_propre_don = donation('passif_propre_don', period)
+        assurance_vie_don = donation('assurance_vie_don', period)
+        return (
+            (1 - part_epoux_don)
+            * (
+                (actif_de_communaute_don - passif_de_communaute_don) / 2
+                + actif_propre_don
+                - passif_propre_don
+                - assurance_vie_don
+                )
+            )
+
+class actif_propre_don(Variable):
+    value_type = float
+    entity = Donation
+    label = "Actif propre"
+    definition_period = ETERNITY
+
+class assurance_vie_don(Variable):
+    value_type = float
+    entity = Donation
+    label = "Assurance Vie"
+    definition_period = ETERNITY
+
+class epoux_survivant_don(Variable):
     value_type = bool
     entity = Donation
     label = "Présence d'un époux survivant"
     definition_period = ETERNITY
 
     def formula(donation, period, parameters):
-        return donation.nb_persons(donation.EPOUX_SURVIVANT)
+        return donation.nb_persons(donation.EPOUX_SURVIVANT_DON)
 
-class nombre_enfants(Variable):
+class nombre_enfants_don(Variable):
     value_type = float
     entity = Donation
     label = "Nombre d'enfants"
@@ -57,7 +97,7 @@ class nombre_enfants(Variable):
     def formula(donation, period, parameters):
         return donation.sum(donation.members('is_enfant', period))
 
-class nombre_freres_soeurs(Variable):
+class nombre_freres_soeurs_don(Variable):
     value_type = float
     entity = Donation
     label = "Nombre de frères et soeurs"
@@ -66,53 +106,57 @@ class nombre_freres_soeurs(Variable):
     def formula(donation, period, parameters):
         return donation.sum(donation.members('is_freres_soeurs', period))
 
-class nombre_autre(Variable):
-    value_type = float
-    entity = Donation
-    label = "Nombre de tiers (personnes autre)"
-    definition_period = ETERNITY
-
-    def formula(donation, period, parameters):
-        return donation.sum(donation.members('is_autre', period))
-class part_epoux(Variable):
+class part_epoux_don(Variable):
     value_type = float
     entity = Donation
     label = "Part epoux"
     definition_period = ETERNITY
 
-class part_taxable(Variable):
+class part_taxable_don(Variable):
     value_type = float
     entity = Donation
     label = "Part taxable"
     definition_period = ETERNITY
 
     def formula(donation, period, parameters):
-        actif_imposable = donation('actif_imposable', period)
-        nombre_enfants = donation('nombre_enfants', period)
-        nombre_freres_soeurs = donation('nombre_freres_soeurs', period)
+        actif_imposable_don = donation('actif_imposable_don', period)
+        nombre_enfants_don = donation('nombre_enfants_don', period)
+        nombre_freres_soeurs_don = donation('nombre_freres_soeurs_don', period)
 
         abattement = parameters(period).abattement
         abattement_epoux_survivant = abattement.abattement_epoux.abattement_epoux_donation
         abattement_enfant = parameters(period).abattement.abattement_enfants.abattement_enfants_donation
         abattement_freres_soeurs = parameters(period).abattement.abattement_freres_soeurs
 
-        epoux_survivant = donation('epoux_survivant', period)
-        enfants = nombre_enfants > 0
-        freres_soeurs = nombre_freres_soeurs > 0
+        epoux_survivant_don = donation('epoux_survivant_don', period)
+        enfants = nombre_enfants_don > 0
+        freres_soeurs = nombre_freres_soeurs_don > 0
 
-        part_taxable_epoux_survivant = max_(actif_imposable - abattement_epoux_survivant, 0)
-        part_taxable_enfant = max_(actif_imposable / (nombre_enfants + 1 * (nombre_enfants == 0)) - abattement_enfant, 0)
-        part_taxable_freres_soeurs = max_(actif_imposable - abattement_freres_soeurs, 0)
+        part_taxable_epoux_survivant_don = max_(actif_imposable_don - abattement_epoux_survivant, 0)
+        part_taxable_enfant_don = max_(actif_imposable_don / (nombre_enfants_don + 1 * (nombre_enfants_don == 0)) - abattement_enfant, 0)
+        part_taxable_freres_soeurs_don = max_(actif_imposable_don - abattement_freres_soeurs, 0)
 
         return select(
             [
-                epoux_survivant > 0,
+                epoux_survivant_don> 0,
                 enfants > 0,
                 freres_soeurs > 0,
                 ],
             [
-                part_taxable_epoux_survivant,
-                part_taxable_enfant,
-                part_taxable_freres_soeurs,
+                part_taxable_epoux_survivant_don,
+                part_taxable_enfant_don,
+                part_taxable_freres_soeurs_don,
                 ],
-            )
+        )
+
+class passif_de_communaute_don(Variable):
+    value_type = float
+    entity = Donation
+    label = "Passif de Communauté"
+    definition_period = ETERNITY
+
+class passif_propre_don(Variable):
+    value_type = float
+    entity = Donation
+    label = "Passif propre"
+    definition_period = ETERNITY
