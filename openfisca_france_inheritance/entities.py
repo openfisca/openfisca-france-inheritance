3b0e1f4c57eadc920683aac58f9ac058193e2ec8
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2023-11-09 22:03:19 +0100

    Again

---
+++ openfisca_france_inheritance/entities.py
@@ -0,0 +1,141 @@
+from openfisca_core.entities import build_entity
+
+
+Individu = build_entity(
+    key = "individu",
+    plural = "individus",
+    label = 'Une personne/un individu',
+    is_person = True,
+    )
+
+
+Succession = build_entity(
+    key = "succession",
+    plural = "successions",
+    label = 'Les individus impliqués dans une succession.',
+    roles = [
+        {
+            'key': 'decede',
+            'label': 'Décédé',
+            'max': 1,
+            'doc': 'La personne décédé.'
+            },
+        {
+            'key': 'epoux_survivant',
+            'label': 'Époux survivant',
+            'max': 1,
+            'doc': "L'époux de la personne décédé."
+            },
+        {
+            'key': 'pacse',
+            'label': 'Partenaires liés par un PACS',
+            'max': 1,
+            'doc': "Le partenaire,lié par un PACS, de la personne décédé."
+            },
+        {
+            'key': 'enfant_survivant',
+            'label': 'Enfants survivants',
+            'doc': "Les enfants vivants au décès de leur parent décédé."
+            },
+        {
+            'key': 'frere_soeur',
+            'label': 'Frères et Soeurs',
+            'doc': "Les frères et soeurs vivants du décédé."
+            },
+        {
+            'key': 'parent',
+            'label': 'Parents',
+            'doc': "Les parents vivants du décédé."
+            },
+        {
+            'key': 'grand-parent',
+            'label': 'Grands-Parents',
+            'doc': "Les grands-parents vivants du décédé."
+            },
+        {
+            'key': 'arrière_grand_parent',
+            'label': 'Arrières-Grands-Parents',
+            'doc': "Les arrières-grands-parents vivants du décédé."
+            },
+        {
+            'key': 'neveu_niece',
+            'label': 'Neveux et Nièces',
+            'doc': "Les neveux et nièces vivants du décédé."
+            },
+        {
+            'key': 'autre_survivant',
+            'label': 'Tiers',
+            'doc': "Légation du patrimoine à un tiers hors du cercle familiale."
+            },
+        ]
+    )
+
+
+Donation = build_entity(
+    key = "donation",
+    plural = "donations",
+    label = 'Les individus impliqués dans une donation.',
+    roles = [
+        {
+            'key': 'donateur',
+            'label': 'Donateur',
+            'max': 1,
+            'doc': 'La personne donatrice.'
+            },
+        {
+            'key': 'epoux_donataire',
+            'label': 'Époux donataire',
+            'max': 1,
+            'doc': "L'époux de la personne donateur."
+            },
+        {
+            'key': 'pacs_donataire',
+            'label': 'Partenaires liés par un PACS',
+            'max': 1,
+            'doc': "Le partenaire,lié par un PACS, de la personne donateur."
+            },
+        {
+            'key': 'enfant_donataire',
+            'label': 'Enfants donataires',
+            'doc': "Les enfants vivants au décès de leur parent donateur."
+            },
+        {
+            'key': 'frere_soeur_donataire',
+            'label': 'Frères et Soeurs donataires',
+            'doc': "Les frères et soeurs vivants du donateur."
+            },
+        {
+            'key': 'parent_donataire',
+            'label': 'Parents donataires',
+            'doc': "Les parents vivants du donateur."
+            },
+        {
+            'key': 'grand_parent_donataire',
+            'label': 'Grands-Parents donataires',
+            'doc': "Les grands-parents vivants du donateur."
+            },
+        {
+            'key': 'arrière_grand_parent_donataire',
+            'label': 'Arrières-Grands-Parents donataires',
+            'doc': "Les arrières-grands-parents vivants du donateur."
+            },
+        {
+            'key': 'neveu_niece_donataire',
+            'label': 'Neveux et Nièces donataires',
+            'doc': "Les neveux et nièces vivants du donateur."
+            },
+        {
+            'key': 'petit_enfant_donataire',
+            'label': 'Petits-Enfants donataires',
+            'doc': "Les petits-enfants vivants au décès de leur parent donateur."
+            },
+        {
+            'key': 'arriere_petit_enfant_donataire',
+            'label': 'Arrières-Petits-Enfants donataires',
+            'doc': "Les arrières-petits-enfants vivants au décès de leur parent donateur."
+            },
+        ]
+    )
+
+
+entities = [Individu, Succession, Donation]
---
+++ openfisca_france_inheritance/scenarios.py
@@ -0,0 +1,121 @@
+from datetime import date
+import logging
+import numpy as np
+
+from openfisca_core import periods, simulations
+
+from openfisca_france_inheritance.individu import ROLE_REPRESENTANT
+
+log = logging.getLogger(__name__)
+N_ = lambda message: message
+
+
+def add_member(entity, **variables_value_by_name):
+    entity.count += 1
+    entity.step_size += 1
+    member_index = entity.count - 1
+    simulation = entity.simulation
+
+    # Add a cell to all arrays of all variables of entity.
+    for variable_name, variable_holder in entity.holder_by_name.iteritems():
+        value_type = variable_holder.column
+        if column.is_permanent:
+            variable_holder._array = np.hstack((variable_holder._array, [column.default]))
+        else:
+            array_by_period = variable_holder._array_by_period
+            if array_by_period is None:
+                variable_holder._array_by_period = array_by_period = {}
+            for period, array in array_by_period.iteritems():
+                array_by_period[period] = np.hstack((array, [column.default]))
+
+    # When entity is a person, ensure that the index & role of the person in the other entities are set.
+    value_by_name = variables_value_by_name.copy()
+    if entity.is_persons_entity:
+        for other_entity in simulation.entity_by_key_singular.itervalues():
+            if not other_entity.is_persons_entity:
+                assert other_entity.count > 0
+                value_by_name.setdefault(other_entity.index_for_person_variable_name, other_entity.count - 1)
+                if other_entity.role_for_person_variable_name is not None:
+                    role = value_by_name.get(other_entity.role_for_person_variable_name)
+                    assert role is not None, "Missing role {} in person arguments: {}".format(
+                        other_entity.role_for_person_variable_name, value_by_name)
+                    if role >= other_entity.roles_count:
+                        other_entity.roles_count = role + 1
+
+    # Set arguments in variables.
+    for variable_name, value in value_by_name.iteritems():
+        variable_holder = entity.get_or_new_holder(variable_name)
+        value_type = variable_holder.column
+        if isinstance(value, dict):
+            for period, period_value in value.iteritems():
+                array = variable_holder.get_array(period)
+                if array is None:
+                    array = np.empty(entity.count, dtype = column.dtype)
+                    array.fill(column.default)
+                    variable_holder.set_array(period, array)
+                array[member_index] = period_value
+        else:
+            period = simulation.period
+            array = variable_holder.get_array(period)
+            if array is None:
+                array = np.empty(entity.count, dtype = column.dtype)
+                array.fill(column.default)
+                variable_holder.set_array(period, array)
+            array[member_index] = value
+
+    return member_index
+
+
+def init_single_succession(debug = False, trace = False, individus = None, succession = None, tax_benefit_system = None,
+        year = None):
+    assert isinstance(individus, list)
+    individus = [
+        individu.copy()
+        for individu in individus
+        ]
+    succession = {} if succession is None else succession.copy()
+    if id is None:
+        succession['id'] = 'succ0'
+
+    decede_count = 0
+    individus_id = set()
+    individu_index_by_id = {}
+    for individu_index, individu in enumerate(individus):
+        individu_id = individu.get('id')
+        assert isinstance(individu_id, basestring) and individu_id not in individus_id
+        individus_id.add(individu_id)
+        individu_index_by_id[individu_id] = individu_index
+        role_representant = individu.get('role_representant')
+        assert role_representant in ROLE_REPRESENTANT._nums, "Individu {} a un rôle invalide: {}".format(
+            individu, role_representant).encode('utf-8')
+        if role_representant == 'décédé':
+            decede_count += 1
+            assert individu.get('id_represente') is None, "Le décédé ne doit représenter personne"
+            individu['id_represente'] = individu_id
+            date_deces = individu.get('date_deces')
+            assert isinstance(date_deces, date), "Le décédé doit avoir une date de décés"
+            individu['quisucc'] = 0  # 'decede'
+        else:
+            individu['quisucc'] = 1  # 'succedant'
+        # Convert role to an integer.
+        individu['role_representant'] = ROLE_REPRESENTANT[individu['role_representant']]
+    assert decede_count == 1, "La simulation doit avoir un et un seul décédé au lieu de {}".format(decede_count)
+
+    for individu in individus:
+        id_represente = individu.get('id_represente')
+        assert id_represente in individus_id, "La personne représentée par l'individu {} n'existe pas".format(
+            individu).encode('utf-8')
+        individu['index_represente'] = individu_index_by_id[id_represente]
+
+    period = periods.period('year', year)
+    simulation = simulations.Simulation(period = period, tax_benefit_system = tax_benefit_system,
+        debug = debug, trace = trace)
+
+    sucession_entity = simulation.entity_by_key_singular['succession']
+    add_member(sucession_entity, **succession)
+
+    individu_entity = simulation.entity_by_key_singular['individu']
+    for individu in individus:
+        add_member(individu_entity, **individu)
+
+    return simulation
