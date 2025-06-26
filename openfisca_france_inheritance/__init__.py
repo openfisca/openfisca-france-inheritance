4c8d7e07fe979031462aa4a3ab334c7cbb7e84a5
sandcha <sandcha@users.noreply.github.com>
2025-06-26 12:56:49 +0200

    Ajoute les exemples d'usage d'API web à open_api_config du TaxBenefitSystem

--- openfisca_france_inheritance/__init__.py
+++ openfisca_france_inheritance/__init__.py
@@ -3,6 +3,7 @@ import os
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
 from openfisca_france_inheritance.entities import entities
+from openfisca_france_inheritance.situation_examples import donation
 
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,6 +13,14 @@ class CountryTaxBenefitSystem(TaxBenefitSystem):
 
     def __init__(self):
         TaxBenefitSystem.__init__(self, entities)
+
         parameters = os.path.join(COUNTRY_DIR, 'parameters')
         self.load_parameters(parameters)
+
         self.add_variables_from_directory(os.path.join(COUNTRY_DIR, 'variables'))
+
+        self.open_api_config = {
+            'variable_example': 'droits_mutation',
+            'parameter_example': 'droits_mutation_titre_gratuit.exoneration.exoneration_don_familial',
+            'simulation_example': donation,
+        }
